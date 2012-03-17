4d7310c699996cd2cc6ca8adeb01540c3e5f0c57
Muhd Amirul Ashraf <amirul@amirul-laptop.(none)>
2012-03-17 11:51:30 +0800

    Added the totitlecase to repo.

---
+++ static/to-title-case.js
@@ -0,0 +1,23 @@
+﻿
+var smallword=["bin","binti","to","a","for","in"];
+
+
+function is_in_small(text){
+  var i=0;
+  while(i<smallword.length){
+    if(smallword[i]==text.toLowerCase()){
+      return true;
+    }
+    i=i+1;
+  }
+  return false;
+}
+
+String.prototype.toTitleCase = function () {
+    return this.replace(/\w\S*/g, function(txt){
+      if(is_in_small(txt)){
+	return txt.toLowerCase();
+      }
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}
+    );
+};
