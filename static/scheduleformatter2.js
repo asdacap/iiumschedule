2534a31973706c8a41308b7836faab1036fd2354
Muhd Amirul Ashraf <asdacap@gmail.com>
2013-02-08 23:00:50 +0800

    Added a warning message if using the message on the portal main page.

--- static/scheduleformatter2.js
+++ static/scheduleformatter2.js
@@ -78,6 +78,20 @@ function start(){
 }
 
 function parsetable() {
+
+    makemessage("Validating path");
+
+    if(/^\/portal\/page\/portal\//.exec(window.location.pathname)){
+        if(!$("#iiumschedulediv").length){
+            $("body").append("<div id='iiumschedulediv' style='width:100%;text-align:center;margin-top:10px;'></div>");
+        }
+        var maindiv=$("#iiumschedulediv");
+        maindiv.append("<h3>Wrong Usage</h3>");
+        maindiv.append("Please use the code on the CRS/<a target='_blank' href='/phpaps/run_rep_list.php?rep_key=confslip1&keepThis=true&TB_iframe=true&height=630&width=900'>Confirmation Slip</a> page.");
+        maindiv.append("<br />Not on the main portal page.");
+        return;
+    }
+
 	// parse table
 	makemessage("Parsing table, please wait...");
 
