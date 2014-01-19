d7ad49649b0ccbe767604dd7b654c7c32e85bb64
Muhd Amirul Ashraf <asdacap@gmail.com>
2014-01-19 13:30:22 +0800

    Now will get debug information from staticsettings.

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import scheduleformatter
 import admincontroller
 import themehandler
 from bootstrap import app
+from staticsettings import DEBUG
+
+bootstrap.app.debug=DEBUG
 
 if(__name__=='__main__'):
     bootstrap.app.run(debug=True)
--- app.wsgi
+++ app.wsgi
@@ -3,4 +3,3 @@ import sys
 sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
 
 from app import app as application
-application.debug=True
